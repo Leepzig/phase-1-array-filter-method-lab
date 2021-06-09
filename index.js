bf2d04c642afc292e7dd71f9e1d5548ba7763a10
andrew <neon.duncan@gmail.com>
2021-06-09 16:17:23 -0500

    commit tree ab3b6e0a34559c58ae06619f39039b92c4a2f69c

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+const findMatching = (arrofDrivers, driver) => {
+  return arrofDrivers.filter(name => name.toLowerCase() === driver.toLowerCase())
+}
+
+const fuzzyMatch = (arrofDrivers, firstLetters) => {
+return arrofDrivers.filter(driverName => driverName[0] + driverName[1] === firstLetters)
+}
+
+// const fuzzyMatch = (arrofDrivers, firstLetters) => {
+//   return arrofDrivers.filter((driverName) => {
+//     driverName[0] + driverName[1] === firstLetters
+//   })
+// }
+
+
+const matchName = (driversArr, driver) => {
+  return driversArr.filter(driverObj => driverObj.name === driver)
+}
